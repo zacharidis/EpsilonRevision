a22857239194e1d2ecc8b3bea0c3911ec1a253c0
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 15:37:37 +0200

    Visual Studio bug that prohibits visibility of DbContext

--- EpsilonRevision.EF/Context/HealthDbContext.cs
+++ EpsilonRevision.EF/Context/HealthDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace EpsilonRevision.EF.Context
 {
-    public  class HealthDbContext :DbContext
+    public  class HealthDbContext: DbContext 
     {
 
     }
