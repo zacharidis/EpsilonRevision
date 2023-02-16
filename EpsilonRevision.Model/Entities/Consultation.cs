68a8caa1f81f1858123f1cab136578df272f4825
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 15:18:49 +0200

    Consultation

--- EpsilonRevision.Model/Entities/Consultation.cs
+++ EpsilonRevision.Model/Entities/Consultation.cs
@@ -8,5 +8,30 @@ namespace EpsilonRevision.Model.Entities
 {
     public class Consultation
     {
+        public int ConsultationId { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Diagnosis { get; set; }
+
+        public string Treatment { get; set; }
+
+       
+
+        public Consultation() { }
+
+        public Consultation(Doctor doctor, string diagnosis, string treatment , Patient patient)
+        {
+            Doctor = doctor;
+            Diagnosis = diagnosis;
+            Treatment = treatment;
+            Date = DateTime.Now;
+            Patient = patient;
+        }
+
+
+        //relationships
+        public Doctor Doctor { get; set; }
+        public Patient Patient { get; set; }
+
     }
 }
