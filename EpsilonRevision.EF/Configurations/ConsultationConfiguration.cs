553fb2ffc33fe5f2caeb7153f0e97b9795029a4a
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 16:40:05 +0200

    Added impl inside the Consultation Configuration

---
+++ EpsilonRevision.EF/Configurations/ConsultationConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EpsilonRevision.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonRevision.EF.Configurations
+{
+    public  class ConsultationConfiguration : IEntityTypeConfiguration<Consultation>
+    {
+        public void Configure(EntityTypeBuilder<Consultation> builder)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
