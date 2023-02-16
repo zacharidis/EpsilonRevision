9cfa6f61f9131e89e9cf02060809c8ed207a2d53
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 15:43:38 +0200

    DbContext class

--- EpsilonRevision.EF/Context/HealthDbContext.cs
+++ EpsilonRevision.EF/Context/HealthDbContext.cs
@@ -3,12 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EpsilonRevision.Model.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EpsilonRevision.EF.Context
 {
     public  class HealthDbContext: DbContext 
     {
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
 
+        public DbSet<Consultation> Consultations { get; set; } = null;
+
+        public DbSet<HospitalAdmission> HospitalAdmissions { get; set; } = null;
+   
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+
+        }
+    
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=HealthDb;Trusted_Connection=True;");
+        }
+    
+    
+    
+    
     }
+
+      
+
 }
