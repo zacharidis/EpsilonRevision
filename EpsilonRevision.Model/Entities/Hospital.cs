69d34f2a9e0236ebdad5f92a136df0154d459636
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 15:11:34 +0200

    Added Hospital Constructor

--- EpsilonRevision.Model/Entities/Hospital.cs
+++ EpsilonRevision.Model/Entities/Hospital.cs
@@ -13,5 +13,12 @@ namespace EpsilonRevision.Model.Entities
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
 
+        public Hospital(string name, string address, string phoneNumber)
+        {
+            Name = name;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+
     }
 }
