a0d531bfcaf22319004954a012b2e69d39514ec4
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 16:21:59 +0200

    Added Surname property

--- EpsilonRevision.Model/Entities/Doctor.cs
+++ EpsilonRevision.Model/Entities/Doctor.cs
@@ -10,6 +10,8 @@ namespace EpsilonRevision.Model.Entities
     {
         public int DoctorId { get; set; }
         public string Name { get; set; }
+
+        public string Surname { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string Specialization { get; set; }
