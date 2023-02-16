f9cf6a148d12fe4366af62f3e9f733425beb8455
Georgios Zacharidis <zageo@outlook.com>
2023-02-16 15:04:33 +0200

    HospitalAdmission finished

--- EpsilonRevision.Model/Entities/HospitalAdmission.cs
+++ EpsilonRevision.Model/Entities/HospitalAdmission.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EpsilonRevision.Model.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,37 @@ namespace EpsilonRevision.Model.Entities
 {
     public class HospitalAdmission
     {
+        
+        public int Id { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public DateTime DischargeDate { get; set; }
+        public string Diagnosis { get; set; }
+        public string Treatment { get; set; }
+
+        public bool IsDischarged { get; set; }
+
+        //enum type
+        public Clinic Clinic { get; set; } 
+
+
+        public HospitalAdmission( Clinic clinic , string diagnosis) {
+
+            AdmissionDate = DateTime.Now;
+            Clinic = clinic;
+            Diagnosis = diagnosis;
+            IsDischarged = false;
+        
+
+            
+        }
+
+
+        //relationships
+
+        
+        public Patient Patient { get; set; }
+
+
+
     }
 }
